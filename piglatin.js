9822315e18c26c4edfb2b0d010ab7461003a7370
Ka Wai <woo.kawai@gmail.com>
2017-10-23 14:04:45 +0800

    Update piglatin.js

--- piglatin.js
+++ piglatin.js
@@ -1,14 +1,17 @@
 function translatePigLatin(str) {
   var vowel = ['a', 'e', 'i', 'o', 'u'];
   var theChar = str.charAt(0);
+  //regular expression for consonant clusters
   var consonant = /[bcdfghjklmnpqrstvwxyz]{1,}/;
   
+  //compares first letter of string to vowel array
   if(vowel.indexOf(theChar) != -1){
     var splitStr = str.split('');
     splitStr.push('way');
     return splitStr.join('');
   } 
   
+  //situation where first letter of a string is consonant
   else if(vowel.indexOf(theChar) == -1){
     if(theChar.search(consonant) != -1 && str.charAt(1).search(consonant) == -1){
       str = str.substr(1) + str.charAt(0) + 'ay';   
@@ -17,12 +20,8 @@ function translatePigLatin(str) {
     if(theChar.search(consonant) != -1 && str.charAt(1).search(consonant) != -1){
       str = str.substr(2) + str.charAt(0) + str.charAt(1) + 'ay';   
      return str;
-    
-  }
-    
- 
- }                                                                   
-                                                                          
-  
+    }
+   }                                                                   
+                                                                   
 } 
 
